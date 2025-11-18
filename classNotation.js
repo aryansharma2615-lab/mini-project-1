50873180ee69c4a6b7972fbfa3227bfab128862a
aryansharma2615-lab <aryan.sharma2615@gmail.com>
2025-11-17 21:08:05 -0500

    Update classNotation.js

--- classNotation.js
+++ classNotation.js
@@ -1,5 +1,5 @@
 // -------------------------------------------
-// Assignment Class
+// Assignment class
 // -------------------------------------------
 class Assignment {
     constructor(title, dueDate) {
@@ -8,13 +8,13 @@ class Assignment {
     }
 
     printAssignment() {
-        console.log(`Title: ${this.title}, Due Date: ${this.dueDate}`);
+        console.log(`   Title: ${this.title} | Due Date: ${this.dueDate}`);
     }
 }
 
 
 // -------------------------------------------
-// Course Class
+// Course class
 // -------------------------------------------
 class Course {
     constructor(courseName, instructor, creditHours, assignments = []) {
@@ -25,22 +25,25 @@ class Course {
     }
 
     courseInfo() {
-        console.log(`Course: ${this.courseName}`);
-        console.log(`Instructor: ${this.instructor}`);
-        console.log(`Credit Hours: ${this.creditHours}`);
-        console.log("Assignments:");
+        console.log(
+            `Course: ${this.courseName} | Instructor: ${this.instructor} | Credit Hours: ${this.creditHours}`
+        );
+        console.log("Assignments >>>");
         this.assignments.forEach(a => a.printAssignment());
     }
 }
 
 
 // -------------------------------------------
-// Create objects (match plainObjects.js)
+// Create objects and print
 // -------------------------------------------
-const a1 = new Assignment("Homework 1", "2025-02-10");
-const a2 = new Assignment("Lab Report", "2025-02-14");
 
-const c1 = new Course("Data Structures", "Dr. Smith", 3, [a1, a2]);
+const assignment1 = new Assignment("Project Proposal", "Jan 15");
+const assignment2 = new Assignment("Midterm Report", "Feb 20");
 
-// Print output
-c1.courseInfo();
+const course = new Course("Software Engineering", "Dr. Pepper", 3, [
+    assignment1,
+    assignment2,
+]);
+
+course.courseInfo();
